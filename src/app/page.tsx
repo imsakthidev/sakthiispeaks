44d0895fb423cbec30dbf3ef7c423cb54688a0b7
sakthi <imsakthidev@gmail.com>
2026-07-19 20:05:44 +0530

    dynamic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic';
+
 import Navbar from '@/components/Navbar/Navbar';
 import Hero from '@/components/Hero/Hero';
 import About from '@/components/About/About';
