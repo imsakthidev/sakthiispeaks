6dcd297faf010eeecb090ce0eab35e6c5e86e7b2
sakthi <imsakthidev@gmail.com>
2026-07-19 20:23:54 +0530

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic';
+
 import type { Metadata } from "next";
 import { Inter, Outfit } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
